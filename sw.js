3eb7b749fb4b95ee8bb71113e49166d117497526
Antonius62 <36734659+Antonius62@users.noreply.github.com>
2025-03-06 20:27:23 +0100

    Add files via upload
    "Erster Entwurf: Lars vom Mars App-Code"

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'lars-vom-mars-v1';
+const ASSETS = [
+  '/',
+  '/app.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
