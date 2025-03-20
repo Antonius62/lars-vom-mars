24ed671c99152d7041a3600acab851e3a8af50ab
Antonius62 <36734659+Antonius62@users.noreply.github.com>
2025-03-20 17:56:55 +0100

    "Text-Position über Buttons behoben"

--- docs/sw.js
+++ docs/sw.js
@@ -1,32 +1,51 @@
 const CACHE_NAME = 'lars-vom-mars-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './Lars_App_Icon.png',
-  './mars_background.png',
-  './quack_sound.mp3',
-  './chapter1.mp3',
-  './chapter2.mp3',
-  './chapter3.mp3'
-];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './lars_vom_mars_song.wav',
+                './quack_sound.mp3',
+                './mars_background.png',
+                './Lars_App_Icon.png',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Immer einen neuen Request machen
+            return fetch(event.request).then((networkResponse) => {
+                // Cache aktualisieren
+                if (networkResponse) {
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                    });
+                }
+                return networkResponse;
+            }).catch(() => {
+                // Fallback auf Cache wenn offline
+                return response;
+            });
+        })
+    );
 });
